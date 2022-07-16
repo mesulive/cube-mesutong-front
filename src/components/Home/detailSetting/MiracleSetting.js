4877f73930e59bb21fef27100dfd5f4f6be17892
vetan2 <vetan2@naver.com>
2022-07-16 18:17:27 +0900

    Fix style bug

--- src/components/Home/detailSetting/MiracleSetting.js
+++ src/components/Home/detailSetting/MiracleSetting.js
@@ -1,47 +1,48 @@
-import React from 'react';
-import {Checkbox, FormControlLabel, Stack} from "@mui/material";
+import React from "react";
+import { Box, Checkbox, FormControlLabel, makeStyles } from "@material-ui/core";
 
-const MiracleSetting = ({input, onSetMiracle}) => {
-  return (
-    input.type === 'TIER' ?
-      <Stack
-        sx={{
-          width: '100%',
-          marginTop: '16px'
-        }}
-        direction="row"
-        justifyContent="center"
-      >
-        <FormControlLabel
-          label="미라클 타임"
-          control=
-            {
-              <Checkbox
-                checked={input.miracle}
-                onChange={(event) => {
-                  onSetMiracle(event.target.checked)
-                }}
-                sx={{
-                  color: 'black',
-                  ...(input.miracle && {color: '#FF7272'}),
-                  '&.Mui-checked': {
-                    color: '#FF7272'
-                  }
-                }}
-              />
-            }
-          sx={{
-            '& .MuiTypography-root': {
-              fontFamily: 'Noto Sans KR',
-              fontWeight: 500,
-              fontSize: '18px',
-              ...(input.miracle && {color: '#FF7272'}),
-              userSelect: 'none'
-            }
-          }}
-        />
-      </Stack> : null
-  );
+const useStyles = makeStyles({
+  root: {
+    color: "black",
+  },
+  checked: {
+    color: "#FF7272 !important",
+  },
+  label: {
+    fontFamily: "Noto Sans KR",
+    fontWeight: 500,
+    fontSize: "18px",
+    userSelect: "none",
+    color: (props) => (props.input.miracle ? "#FF7272" : "black"),
+  },
+});
+
+const MiracleSetting = ({ input, onSetMiracle }) => {
+  const classes = useStyles({ input, onSetMiracle });
+  return input.type === "TIER" ? (
+    <Box
+      style={{
+        width: "100%",
+        marginTop: "16px",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <FormControlLabel
+        label="미라클 타임"
+        control={
+          <Checkbox
+            checked={input.miracle}
+            onChange={(event) => {
+              onSetMiracle(event.target.checked);
+            }}
+            classes={{ root: classes.root, checked: classes.checked }}
+          />
+        }
+        classes={{ label: classes.label }}
+      />
+    </Box>
+  ) : null;
 };
 
 export default MiracleSetting;
